c20fe4ba9720416971fe326cfc921034bd7ba70f
hexaend <hexaend@mail.ru>
2026-04-02 00:54:12 +0300

    added AbstractEntity and fix some bugs(Lady) (... hashCode and equals :) )

--- src/main/java/ru/hexaend/entity/AbstractEntity.java
+++ src/main/java/ru/hexaend/entity/AbstractEntity.java
@@ -13,22 +13,42 @@ public class AbstractEntity<ID> {
     protected AbstractEntity(ID id) {
         this(id, LocalDateTime.now(), LocalDateTime.now());
     }
+
     protected AbstractEntity(ID id, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = Objects.requireNonNull(id, "id must not be null");
         this.createdAt = Objects.requireNonNull(createdAt, "createdAt must not be null"); // хм.. а есть ли тут в этом смысл???
         this.updatedAt = Objects.requireNonNull(updatedAt, "updatedAt must not be null");// мб, просто вешать на них now???
     }
+
     public ID getId() {
         return this.id;
     }
+
     public LocalDateTime getCreatedAt() {
         return this.createdAt;
     }
+
     public LocalDateTime getUpdatedAt() {
         return this.updatedAt;
     }
+
     public void markAsUpdated() {
         this.updatedAt = LocalDateTime.now();
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof AbstractEntity<?> other)) { // лол, java автоматом присваивает other для o 0.0
+            return false;
+        }
+        return id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
